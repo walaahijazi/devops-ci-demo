30084c4b18253aa32bcb4f835b07e1f7716cdd57
George Khoury <g@gkhoury.me>
2020-04-09 09:16:15 +0300

    Changed to a simple NGINX web project.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+set -e
+
 # Define some path
 SRC=./web
-DST="/var/ww/html"
+DST=/var/ww/html
 
 # Check if deployment destination exists
 if [ -d "$DST" ]; then
+    echo "Deployment target exists: $DST"
     cp -r $SRC/* $DST
+else
+    echo "Deployment target does not exist."
+    exit 2
 fi

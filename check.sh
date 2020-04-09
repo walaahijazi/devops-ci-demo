0aab82b6c8f5db58d707ceb46b894b9c7e0c6a9b
George Khoury <g@gkhoury.me>
2020-04-09 09:07:46 +0300

    Changed to a simple NGINX web project.

--- check.sh
+++ check.sh
@@ -1,2 +1,14 @@
 #! /bin/bash
 
+# Check if NGINX is installed
+if ! which nginx > /dev/null 2>&1; then
+    echo "NGINX not installed."
+    sudo apt install nginx -y
+else
+    echo "NGINX v$(nginx -v) is installed."
+fi
+
+# Check NGINX user
+
+
+

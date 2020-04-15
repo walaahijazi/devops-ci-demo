a1660853b5e4db481441705c746d055eaab1f4dd
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-15 23:01:23 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ echo "running this build script"
 echo "i am $HOSTNAME"
 echo "the time is $(date +%m-%d-%Y)"
 echo "the logged user is $USER"
-SERVER=$(HOSTNAME)
+
 T=$(date +"%m-%d-%Y")
 GIT-REPO=devops-ci-demo
 GIT-BRANCH=$(git branch | sed -n -e 's/^\* \(. *\)/\1/p')
 PWD=$PWD
 USR=$USER
-sed "s/{SERVER_NAME}/$HOSTNAME /g" web/index.html
+sed "s/{SERVER_NAME}/${HOSTNAME} /g" web/index.html
 sed -i "s/{TIMESTAMP}/$T/g" web/index.html
 sed -i "s/{GIT-BRANCH}/${GIT-BRANCH} /g" web/index.html
 sed -i "s/{GIT-REPO}/${GIT-REPO} /g" web/index.html

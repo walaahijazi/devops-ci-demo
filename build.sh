1d2008b8dcde68e74842c891f950c52b9d1a2bbf
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-15 18:22:58 +0300

    s

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ TIMESSTAMP=$(date +%m-%d-%Y)
 GIT-REPO="devops-ci-demo"
 GIT-BRANCH=$(git branch | sed -n -e 's/^\* \(. *\)/\1/p'
 PWD=$(PWD)
+USER=$name
+sed "s/{SERVER_NAME}/$SERVER_NAME/g"web/index.html
 sed -i "s/{TIMESTAMP}/$TIMESTAMP/g" web/index.html
-
+sed -i "s?{GIT-BRANCH}?$GIT-BRANCH?g" web/index.html
+sed -i "s?{GIT-REPO}?$GIT-REPO?g"web/index.html
+sed -i "s/{PWD}/$PWD/g" web/index.html
+sed -i "s/{USER}/$name/g" web/index.html
 

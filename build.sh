c7a4ed7d800000f1da6bbef03e8e7e3234144236
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-15 17:43:01 +0300

    suub

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-export SERVER_NAME=$(hostname)
-export TIMESSTAMP=$(date +%m-%d-%Y)
-export GIT-REPO="devops-ci-demo"
-export GIT-BRANCH=$(git branch | sed -n -e 's/^\* \(. *\)/\1/p'
-export PWD=$PWD
-sed -i "s/{/\${/g" web/index.html
-envsubst'$TIMESSTAMP $GIT-REPO $GIT-BRANCH $SERVER_NAME $PWD' > web/new index < web/index.html
+SERVER_NAME=$(hostname)
+TIMESSTAMP=$(date +%m-%d-%Y)
+GIT-REPO="devops-ci-demo"
+GIT-BRANCH=$(git branch | sed -n -e 's/^\* \(. *\)/\1/p'
+PWD=$PWD
+sed -i "s/{TIMESTAMP}/$TIMESTAMP/g" new index
 
 

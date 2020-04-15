95d72ef9ed2eabc3029e888bdd98f0f7e162ab7c
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-15 22:52:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ SERVER=$(HOSTNAME)
 T=$(date +"%m-%d-%Y")
 GIT-REPO=devops-ci-demo
 GIT-BRANCH=$(git branch | sed -n -e 's/^\* \(. *\)/\1/p')
-PWD=$(PWD)
-USER=$(USER)
-sed "s/{SERVER_NAME}/${HOSTNAME}/g" web/index.html
+PWD=$PWD
+USR=$USER
+sed "s/{SERVER_NAME}/${HOSTNAME} /g" web/index.html
 sed -i "s/{TIMESTAMP}/$T/g" web/index.html
-sed -i "s/{GIT-BRANCH}/${GIT-BRANCH}/g" web/index.html
-sed -i "s/{GIT-REPO}/${GIT-REPO}/g" web/index.html
-sed -i "s/{PWD}/${PWD}/g" web/index.html
-sed -i "s/{USER}/${USER}/g" web/index.html
+sed -i "s/{GIT-BRANCH}/${GIT-BRANCH} /g" web/index.html
+sed -i "s/{GIT-REPO}/${GIT-REPO} /g" web/index.html
+sed -i "s/{PWD}/${PWD} /g" web/index.html
+sed -i "s/{USER}/${USER} /g" web/index.html
 

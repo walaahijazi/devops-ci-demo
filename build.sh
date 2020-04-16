92ae701e878d87d2821eeb768162e53639c62997
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-16 19:41:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "the logged user is $USER"
 
 T=$(date +"%m-%d-%Y")
 g=devops-ci-demo
-b=gitBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+b=git branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 DIR=$PWD
 name=$USER
 SERVER_NAME=$HOSTNAME

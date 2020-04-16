9266662f9c19763ab964fda304fa571f6a4d39a1
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-16 15:06:53 +0300

    Update build.sh
    SUU

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ echo "the logged user is $USER"
 T=$(date +"%m-%d-%Y")
 g=devops-ci-demo
 b=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-TEMPDIR=$PWD
+DIR=$PWD
 name=$USER
 SERVER_NAME=$HOSTNAME
 sed -i "s/{SERVER_NAME}/${HOSTNAME}/g" web/index.html
 sed -i "s/{TIMESTAMP}/$T/g" web/index.html
 sed -i "s?{GIT-BRANCH}?/$b/g" web/index.html
 sed -i "s/{GIT_REPO}/$g/g" web/index.html
-sed "s@TEMPDIR@$(pwd)@" web/index.html
+sed "s~{PWD}~$DIR" web/index.html
 sed -i "s/{USER}/$name/g" web/index.html
 sed -i "s/{AZURE_VARIABLE}/${AZURE_VARIABLE}/g" web/index.html
 

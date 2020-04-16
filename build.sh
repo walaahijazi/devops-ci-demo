a77e453e803706043fe51db06ee1fb3eb5bf3068
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-16 09:16:24 +0300

    sub

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ echo "the logged user is $USER"
 T=$(date +"%m-%d-%Y")
 GIT-REPO=devops-ci-demo
 GIT-BRANCH=$(git branch | sed -n -e 's/^\* \(. *\)/\1/p')
-PWD=$PWD
-USR=$USER
+DIR=$(PWD)
+name=$USER
+server=$HOSTNAME
 sed "s/{SERVER_NAME}/${HOSTNAME} /g" web/index.html
 sed -i "s/{TIMESTAMP}/$T/g" web/index.html
 sed -i "s/{GIT-BRANCH}/${GIT-BRANCH} /g" web/index.html
 sed -i "s/{GIT-REPO}/${GIT-REPO} /g" web/index.html
-sed -i "s/{PWD}/${PWD} /g" web/index.html
-sed -i "s/{USER}/${USER} /g" web/index.html
+sed -i "s/{PWD}/${DIR} /g" web/index.html
+sed -i "s/{USER}/${name} /g" web/index.html
 

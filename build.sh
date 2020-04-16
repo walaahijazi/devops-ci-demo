b80c352a40206cf88484d64e79f0b58e8d82368e
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-16 13:35:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "the logged user is $USER"
 T=$(date +"%m-%d-%Y")
 g=devops-ci-demo
 b=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-TEMPDIR=$(PWD)
+TEMPDIR=$PWD
 name=$USER
 SERVER_NAME=$HOSTNAME
 sed -i "s/{SERVER_NAME}/${HOSTNAME}/g" web/index.html

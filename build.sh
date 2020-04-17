5c578899645f6888e92066ba690767b7528b642c
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-17 13:11:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ name=$USER
 SERVER_NAME=$HOSTNAME
 sed -i "s/{SERVER_NAME}/${HOSTNAME}/g" web/index.html
 sed -i "s/{TIMESTAMP}/$T/g" web/index.html
-sed -i "s?{GIT_BRANCH}/$b/g" web/index.html
 sed -i "s/{GIT_REPO}/$g/g" web/index.html
 sed -i "s~{PWD}~$DIR~g" web/index.html
 sed -i "s/{USER}/$name/g" web/index.html
